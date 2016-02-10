43e54d0cfc1b70135c4969db6163a5140acf7130
Kristen O'Dell <little_miss_kris07@hotmail.com>
2016-02-09 18:23:12 -0700

    Loops

---
+++ Conventions and Syntax/Loops/Assets/ForLoops.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoops : MonoBehaviour 
+{
+	int numEnemies = 3;
+	
+	void Start()
+	{
+		for(int i = 0; i < numEnemies; i++)
+		{
+			Debug.Log("Creating Enemy Number:" + i);
+		}
+	}
+}
