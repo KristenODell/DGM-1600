5dd07ac67cdbdea53b823da4d5cc103cf96e6a29
Kristen O'Dell <little_miss_kris07@hotmail.com>
2016-03-08 17:54:41 -0700

    Classes and Datatypes

---
+++ DGM 1600 1/Assets/ClassesandDatatypes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassesandDatatypes : MonoBehaviour 
+{
+	public class Stuff
+	{
+		public int bullets;
+		public int grenades;
+		public int rockets;
+		
+		public Stuff (int bul, int gre, int rock)
+		{
+			bullets = bul;
+			grenades = gre;
+			rockets = rock;
+		}
+		
+		public Stuff ()
+		{
+			bullets = 1;
+			grenades = 1;
+			rockets = 1;
+		}
+	}
+	
+	public Stuff myStuff = new Stuff(50, 5, 5);
+	
+	void Start ()
+	{
+		Debug.Log(myStuff.bullets);
+	}
+}
