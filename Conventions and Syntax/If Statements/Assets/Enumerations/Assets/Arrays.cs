3e19a9f16e2e11ba93120ce11ba0c50714b655f9
Kristen O'Dell <10655578@CS518-07.ad.uvu.edu>
2016-03-10 18:01:09 -0700

    Arrays

---
+++ Conventions and Syntax/If Statements/Assets/Enumerations/Assets/Arrays.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour 
+{
+	public GameObject[]players;
+
+	void Start ()
+	{
+		players = GameObject.FindGameObjectsWithTag("Player");
+
+		for(int i = 0; i < players.Length; i++)
+		{
+			Debug.Log("Player number " + (i + 1) + " is named " + players[i].name);
+		}
+	}
+}
