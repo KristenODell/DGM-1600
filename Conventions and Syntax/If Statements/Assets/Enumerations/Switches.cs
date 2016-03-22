1704f383dd01107b46f3c04d85edde678dfb47c7
Kristen O'Dell <little_miss_kris07@hotmail.com>
2016-03-22 17:44:33 -0600

    Switches

---
+++ Conventions and Syntax/If Statements/Assets/Enumerations/Switches.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switches : MonoBehaviour 
+{
+	public int intelligence = 2;
+	
+	void Greet ()
+	{
+		switch(intelligence)
+		{
+		case 2:
+			print("Hello");
+			break;
+		case 1:
+			print("Hi");
+			break;
+		default:
+			print("Error");
+			break;
+		}
+	}
+	void Start()
+	{
+		Greet();
+	}
+}
